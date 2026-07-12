bd152996ebb9d3dfa635e401ec9f2c0c6280bac7
Quim Nuss Székely <4179721+quimnuss@users.noreply.github.com>
2026-07-12 14:33:31 +0200

    tilemaps and player movement animations

--- actors/player/explorer.gd
+++ actors/player/explorer.gd
@@ -1,17 +1,42 @@
 extends CharacterBody2D
+@onready var sprite_2d: Sprite2D = $Anchor/Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+var last_walk_velocity : Vector2 = Vector2.ZERO
 
 const SPEED = 100.0
 
 
 func _physics_process(delta: float) -> void:
+   
+    var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+    if direction.x < 0:
+        sprite_2d.flip_h = true
+    elif direction.x > 0:
+        sprite_2d.flip_h = false
 
-    # Get the input direction and handle the movement/deceleration.
-    # As good practice, you should replace UI actions with custom gameplay actions.
-    var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
     if direction:
         velocity = direction * SPEED
     else:
         velocity = Vector2.ZERO
+
+
+    if velocity.length() > 0:
+        if abs(velocity.x) > 0:
+            animation_player.play("walk_horizontal")
+        elif velocity.y < 0:
+            animation_player.play("walk_up")
+        elif velocity.y > 0:
+            animation_player.play("walk_down")
+        last_walk_velocity = velocity
+    else:
+        if abs(last_walk_velocity.x) > 0:
+            animation_player.play("idle_horizontal")
+        elif last_walk_velocity.y < 0:
+            animation_player.play("idle_up")
+        elif last_walk_velocity.y > 0:
+            animation_player.play("idle_down")
+        else:
+            animation_player.play("idle_horizontal")
 
     move_and_slide()
