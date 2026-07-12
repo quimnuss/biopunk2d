e72a59acd9eec821ca39ad7329e807bd4bdb069c
Quim Nuss Székely <4179721+quimnuss@users.noreply.github.com>
2026-07-12 20:18:27 +0200

    eyeplant, seed, player move animations

---
+++ actors/misc/eyeplant.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@export var target : Player
+@onready var eye_center: Node2D = $EyeCenter
+@onready var eye: Sprite2D = $EyeCenter/Eye
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+    if target:
+        target.global_position - eye_center.global_position
+        eye.global_position = eye_center.global_position.move_toward(target.global_position, 3)
+    
+
+func _on_sensor_body_entered(body: Node2D) -> void:
+    if body is Player:
+        target = body
+        
+
+
+func _on_sensor_body_exited(body: Node2D) -> void:
+    var tween := get_tree().create_tween()
+    tween.tween_property(eye, "position", Vector2.ZERO, 1).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+    target = null
--- actors/player/explorer.gd
+++ actors/player/explorer.gd
@@ -1,3 +1,4 @@
+class_name Player 
 extends CharacterBody2D
 @onready var sprite_2d: Sprite2D = $Anchor/Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
