919d971f35af8b9326081ff5ff12696e5d0c1fe4
Quim Nuss Székely <4179721+quimnuss@users.noreply.github.com>
2026-07-14 00:20:27 +0200

    eyeseed hiding

--- actors/misc/eyeplant.gd
+++ actors/misc/eyeplant.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var target : Player
 @onready var eye_center: Node2D = $EyeCenter
 @onready var eye: Sprite2D = $EyeCenter/Eye
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,3 +26,12 @@ func _on_sensor_body_exited(body: Node2D) -> void:
     var tween := get_tree().create_tween()
     tween.tween_property(eye, "position", Vector2.ZERO, 1).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
     target = null
+
+
+func _on_close_sensor_body_entered(body: Node2D) -> void:
+    if body is Player:
+        animation_player.play("hide")
+
+func _on_close_sensor_body_exited(body: Node2D) -> void:
+    if body is Player:
+        animation_player.play_backwards("hide")
