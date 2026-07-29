a928bc2d796343bb9777d2ce3c99a3c864791525
Quim Nuss Székely <4179721+quimnuss@users.noreply.github.com>
2026-07-29 23:16:40 +0200

    inventory system start

---
+++ misc/item_data.gd
@@ -0,0 +1,8 @@
+class_name BiopunkItem
+extends Resource
+
+@export var item_id : String
+@export var name : String
+@export var texture : Texture
+@export var ui_texture : Texture
+@export var quantity : int = 0
