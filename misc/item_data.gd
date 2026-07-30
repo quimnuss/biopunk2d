bb0b9cc76f0971467a59370cdbe4d6a35b12db89
Quim Nuss Székely <4179721+quimnuss@users.noreply.github.com>
2026-07-30 12:18:49 +0200

    start inventory

--- misc/item_data.gd
+++ misc/item_data.gd
@@ -1,8 +1,14 @@
 class_name BiopunkItem
 extends Resource
 
+enum ItemType {FRUIT}
+enum ItemTag {THROWABLE}
+
 @export var item_id : String
+@export var item_type : ItemType
+@export var tags : ItemTag
 @export var name : String
+@export_multiline var description: String
 @export var texture : Texture
 @export var ui_texture : Texture
 @export var quantity : int = 0
---
+++ misc/item_database.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var items_db : Dictionary[String,BiopunkItem]
