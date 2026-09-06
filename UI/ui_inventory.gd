a18f0b9dd4bb4582e8437757419f8d6521429186
Quim Nuss Székely <4179721+quimnuss@users.noreply.github.com>
2026-09-06 15:13:20 +0200

    inventory ui start

---
+++ UI/ui_inventory.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+@export var inventory_data : Inventory
+
+
+func _ready():
+    #inventory_data.changed.connect(_on_inventory_changed)
+    pass
+func _on_inventory_changed():
+    pass
